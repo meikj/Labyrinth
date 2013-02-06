eea0b84aa749b0ffe6f9a9ef01c2c49e5f97f435
John Meikle <john.meikle31@gmail.com>
2013-02-06 19:13:23 +0000

    Initial directory structure and Eclipse additions to .gitignore

---
+++ src/Labyrinth.java
@@ -0,0 +1,13 @@
+/**
+ * Labyrinth game
+ * 
+ * @author Gareth Gill & John Meikle
+ *
+ */
+public class Labyrinth {
+	
+	public static void main(String[] args) {
+		System.out.println("Labyrinth game!");
+	}
+
+}
