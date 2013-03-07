97d858246f73102e8b394b0538c333460231c970
John Meikle <john.meikle31@gmail.com>
2013-03-07 00:18:09 +0000

    Refactored the entry point code

--- src/Labyrinth.java
+++ src/Labyrinth.java
@@ -6,43 +6,84 @@ import java.util.Scanner;
  * 
  * @author Gareth Gill
  * @author John Meikle
- * @version 0.1.06032013
+ * @version 0.1.07032013
  *
  */
 public class Labyrinth {
 	
-	public static void main(String[] args) {
-		TreasureChase tc = new TreasureChase(new Player());
-		
-		System.out.println("Welcome to the Labyrinth game!\n");
-		
-		while(true) {
-			Scanner sc = new Scanner(System.in);
-			 
+	private TreasureChase tc;
+	private Scanner input;
+	private boolean execute;
+	
+	public Labyrinth() {
+		tc = new TreasureChase(new Player());
+		input = new Scanner(System.in);
+		execute = true;
+	}
+	
+	public void run() {
+		while(execute) {
 			tc.update();
 			 
-			System.out.println("\nPlease enter your spare tile at coordinates <x> <y>:");
-			 
-			String input = sc.nextLine();
-			String[] coordinates = input.split(" ");
-			
+			System.out.print("\n> ");
+			parseInput(promptUser());
+		}
+	}
+	
+	/**
+	 * Parse user input and perform appropriate operation(s)
+	 * 
+	 * @param inputArgs The list of arguments the user passed
+	 */
+	public void parseInput(String[] inputArgs) throws NumberFormatException, IndexOutOfBoundsException {
+		if(inputArgs[0].toLowerCase().equals("insert")) {
+			if(inputArgs.length < 3) {
+				System.out.println("Usage: insert <x> <y>");
+				return;
+			}
+		
+			// Insert spare tile command
 			int x, y;
 			
 			try {
-				x = Integer.parseInt(coordinates[0]);
-				y = Integer.parseInt(coordinates[1]);
+				x = Integer.parseInt(inputArgs[1]);
+				y = Integer.parseInt(inputArgs[2]);
 			}
 			catch(NumberFormatException e) {
-				System.out.println("Invalid coordinates entered\n");
-				continue;
+				throw new NumberFormatException("Invalid coordinates entered\n");
 			}
 			catch(IndexOutOfBoundsException e) {
-				System.out.println("Not enough arguments\n");
-				continue;
+				throw new IndexOutOfBoundsException("Not enough arguments\n");
 			}
-			 
+			
 			tc.replaceTile(x, y);
 		}
+		else if(inputArgs[0].toLowerCase().equals("help")) {
+			System.out.println("insert \t Insert spare tile to specified location");
+			System.out.println("help \t Display available game options");
+		}
+		else {
+			System.out.println("Please enter 'help' for more information");
+		}
+	}
+	
+	/**
+	 * Prompt user for input
+	 * 
+	 * @return The users input in a tokenized form
+	 */
+	public String[] promptUser() {
+		String in = input.nextLine();
+		String[] tokens = in.split(" ");
+		
+		return tokens;
+	}
+	
+	public static void main(String[] args) {
+		Labyrinth game = new Labyrinth();
+		
+		System.out.println("Welcome to the Labyrinth game!\n");
+		game.run();
 	}
 
 }
